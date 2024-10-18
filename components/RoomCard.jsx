f279cec7e6a6d628d49038548f6c1ffac72d4541
andrewteece <andrewteece@gmail.com>
2024-10-18 10:23:40 -0500

    update reademe

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -6,7 +6,7 @@ const RoomCard = ({ room }) => {
     <div className='bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <div className='flex flex-col sm:flex-row sm:space-x-4'>
         <Image
-          src='/images/rooms/room-1.jpg'
+          src={`/images/rooms/${room.image}`}
           width={400}
           height={100}
           alt={room.name}
---
+++ readne.md
@@ -0,0 +1,4 @@
+# Bookit App
+
+Meeting room booking application built with Next.js, Appwrite, and Tailwind CSS.
+<img src="public/images/screen.png" alt="" />
