01ebd3f43a5262c6b11c8cf77056ff56a004b671
andrewteece <andrewteece@gmail.com>
2024-10-20 08:55:53 -0500

    import Heading and use params

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,5 +1,17 @@
-const RoomPage = () => {
-  return <>Room</>
+import Heading from '@/components/Heading'
+import rooms from '@/data/rooms.json'
+
+const RoomPage = ({ params }) => {
+  const { id } = params
+  const room = rooms.find((room) => room.$id === id)
+  if (!room) {
+    return <Heading title='Room Not Found' />
+  }
+  return (
+    <>
+      <Heading title={room.name} />
+    </>
+  )
 }
 
 export default RoomPage
