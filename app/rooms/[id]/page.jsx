7da6ee8fe05458b86e18955031248acc646c0f44
andrewteece <andrewteece@gmail.com>
2024-10-22 06:24:06 -0500

    set dynamic entries from json

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -14,45 +14,45 @@ const RoomPage = ({ params }) => {
     <>
       <Heading title={room.name} />
       <div className='bg-white shadow rounded-lg p-6'>
-        <a
+        <Link
           href='/rooms.html'
           className='flex items-center text-gray-600 hover:text-gray-800 mb-4'
         >
           <FaChevronLeft className='inline mr-1' />
           <span className='ml-2'>Back to Rooms</span>
-        </a>
+        </Link>
 
         <div className='flex flex-col sm:flex-row sm:space-x-6'>
-          <img
-            src='images/rooms/room-1.jpg'
-            alt='Grand Conference Hall'
+          <Image
+            src={`/images/rooms/${room.image}`}
+            alt={room.name}
+            width={400}
+            height={100}
             className='w-full sm:w-1/3 h-64 object-cover rounded-lg'
           />
 
           <div className='mt-4 sm:mt-0 sm:flex-1'>
-            <p className='text-gray-600 mb-4'>
-              Spacious conference hall perfect for large meetings and events.
-              Equipped with modern amenities and comfortable seating.
-            </p>
+            <p className='text-gray-600 mb-4'>{room.description}</p>
 
             <ul className='space-y-2'>
               <li>
-                <span className='font-semibold text-gray-800'>Size:</span> 3000
+                <span className='font-semibold text-gray-800'>Size:</span>{' '}
+                {room.sqft}
                 sq ft
               </li>
               <li>
                 <span className='font-semibold text-gray-800'>
                   Availability:
                 </span>
-                9 AM - 5 PM
+                {room.availability}
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Price:</span>
-                $150/hour
+                <span className='font-semibold text-gray-800'>Price:</span>$
+                {room.price_per_hour}/hour
               </li>
               <li>
                 <span className='font-semibold text-gray-800'>Address:</span>{' '}
-                555 California St, San Francisco, CA 94104
+                {room.address}
               </li>
             </ul>
           </div>
